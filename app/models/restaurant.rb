36c024ee76fb6c4ed467d28b17a979cb06067bb1
Ricardo Ribeiro <ricardoc6r8@gmail.com>
2019-05-22 12:45:07 -0300

    Models and Routes

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,16 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, inclusion: { in: %w[
+    chinese
+    italian
+    japanese
+    french
+    belgian
+    brazilian
+    indian
+    arabic
+    german
+  ] }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    resources :reviews, only: %i[index new create]
+  end
+
+  resources :reviews, only: %i[show edit update destroy]
 end
